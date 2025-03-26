7aa059c31cd86c461d3160c330deddccb28de57c
Kenny McCormick <kennyrkun@users.noreply.github.com>
2025-03-26 12:19:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,33 @@
 import asyncio
 from websockets.asyncio.server import serve
 
+def runDirewolf(text, voice):
+    program = subprocess.Popen(
+        [
+            "ffplay",
+            "-hide_banner",
+            "-loglevel", "error",
+            "-f", "s16le",
+            "-ar", str(voice.config.sample_rate),
+            "-nodisp",
+            "-infbuf",
+            "-autoexit",
+            "-af", f"volume={flags.voiceVolume}",
+            # can't use atempo because it exits too early and cuts off the last phonem.
+            # instead, we use piper's length_scale to change voice speed.
+            "-i", "pipe:"
+        ],
+        stdout=sys.stdout,
+        stderr=subprocess.STDOUT,
+        stdin=subprocess.PIPE
+    )
+    
+    for byteData in voice.synthesize_stream_raw(text):
+        program.stdin.write(byteData)
+    
+    program.stdin.close()
+    program.wait()
+
 async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
